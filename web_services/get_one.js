952104f57b922dcc9b3ea1be8437876421d7365d
pcosta <pcostade@hotmail.com>
2019-11-13 18:48:55 +0000

    fix

--- web_services/get_one.js
+++ web_services/get_one.js
@@ -4,7 +4,7 @@ function app(req, res) {
   const records = pjs.query("select * from requests where id = ?", req.params.id);
 
   if(records.length > 0 ) res.send({found: true, record: records[0]});
-  else res.sen({found: false});
+  else res.send({found: false});
 
 
 
